fafc3f222c5d22038b2e5194716b1907416aaa75
Nazareno Pisoni <nazareno_pisoni@hotmail.com>
2023-05-24 11:29:30 -0300

    Third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import models.ABM;
+import models.Persona;
+
 import java.util.*;
 
 public class Main {
@@ -7,25 +10,25 @@ public class Main {
         //Utilice un HashMap para almacenar los resultados y muestre el mapa al
         //usuario al final.
 
-//        Map<Character, Integer> frecuencias = new HashMap<Character, Integer>();
-//        String cadena;
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Ingrese una cadena de texto: ");
-//        cadena = sc.nextLine();
-//        for(int i=0; i<cadena.length(); i++){
-//            char caracter = cadena.charAt(i);
-//            if(frecuencias.containsKey(caracter)){
-//                frecuencias.put(caracter, frecuencias.get(caracter) + 1);
-//            }else {
-//                frecuencias.put(caracter, 1);
-//            }
-//        }
-//        System.out.println("Frecuencia de caracteres: ");
-//        for(Map.Entry<Character, Integer> entry : frecuencias.entrySet()){
-//            char caracter = entry.getKey();
-//            int frecuencia = entry.getValue();
-//            System.out.println("'" + caracter + "': " + frecuencia);
-//        }
+        Map<Character, Integer> frecuencias = new HashMap<Character, Integer>();
+        String cadena;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una cadena de texto: ");
+        cadena = sc.nextLine();
+        for(int i=0; i<cadena.length(); i++){
+            char caracter = cadena.charAt(i);
+            if(frecuencias.containsKey(caracter)){
+                frecuencias.put(caracter, frecuencias.get(caracter) + 1);
+            }else {
+                frecuencias.put(caracter, 1);
+            }
+        }
+        System.out.println("Frecuencia de caracteres: ");
+        for(Map.Entry<Character, Integer> entry : frecuencias.entrySet()){
+            char caracter = entry.getKey();
+            int frecuencia = entry.getValue();
+            System.out.println("'" + caracter + "': " + frecuencia);
+        }
 
         //2. Escriba un programa que genere una lista de números aleatorios, la ordene y
         //luego elimine los duplicados. Utilice un HashSet para almacenar los números
@@ -42,20 +45,19 @@ public class Main {
         //de personas y sus edades, y luego ordene la lista por edad. Utilice un
         //TreeMap para almacenar los nombres y las edades.
 
-//        String nombre;
-//        int edad;
-//        TreeMap<Integer, String> personas = new TreeMap<Integer, String>();
-//        for(int i=0; i<5; i++){
-//            Scanner sc = new Scanner(System.in);
-//            System.out.println("Ingrese el nombre: ");
-//            nombre = sc.nextLine();
-//            System.out.println("Ingrese la edad: ");
-//            edad = sc.nextInt();
-//            personas.put(edad, nombre);
-//        }
-//        for(Map.Entry<Integer, String> entry : personas.entrySet()){
-//            System.out.println("El nombre es " + entry.getValue() + " y su edad es " + entry.getKey());
-//        }
+        String nombre;
+        int edad;
+        TreeMap<Integer, String> personas = new TreeMap<Integer, String>();
+        for(int i=0; i<5; i++){
+            System.out.println("Ingrese el nombre: ");
+            nombre = sc.nextLine();
+            System.out.println("Ingrese la edad: ");
+            edad = sc.nextInt();
+            personas.put(edad, nombre);
+        }
+        for(Map.Entry<Integer, String> entry : personas.entrySet()){
+            System.out.println("El nombre es " + entry.getValue() + " y su edad es " + entry.getKey());
+        }
 
         //4. Escriba un programa que lea una lista de números enteros y luego calcule la
         //suma y el promedio. Utilice un ArrayList para almacenar los números y una
@@ -64,36 +66,66 @@ public class Main {
         //datos que a través de métodos concatenados podemos filtrarlos, modificarlos
         //y realizar distintas operaciones que devuelvan un flujo totalmente diferente.
 
-//        List<Integer> numeros = new ArrayList<Integer>();
-//        numeros.add(50);
-//        numeros.add(10);
-//        numeros.add(35);
-//        numeros.add(20);
-//        numeros.add(55);
-//        int suma = numeros.stream().mapToInt(Integer::intValue).sum();
-//        System.out.println("La suma de los numeros es: " + suma);
-//        double promedio = numeros.stream().mapToDouble(Integer::doubleValue).average().orElse(0);
-//        System.out.println("El promedio de los numeros es: " + promedio);
+        List<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(50);
+        numeros.add(10);
+        numeros.add(35);
+        numeros.add(20);
+        numeros.add(55);
+        int suma = numeros.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("La suma de los numeros es: " + suma);
+        double promedio = numeros.stream().mapToDouble(Integer::doubleValue).average().orElse(0);
+        System.out.println("El promedio de los numeros es: " + promedio);
 
         //5. Escriba un programa que lea una lista de números enteros y luego ordene la
         //lista en orden ascendente y descendente. Utilice un TreeSet para almacenar
         //los números y mostrarlos en orden ascendente y descendente.
 
-        List<Integer> numeros = new ArrayList<Integer>();
-        numeros.add(18);
-        numeros.add(5);
-        numeros.add(10);
-        numeros.add(17);
-        numeros.add(3);
-        numeros.add(31);
-        Set<Integer> numerosOrdenadosAsc = new TreeSet<>(numeros);
+        List<Integer> listaNros = new ArrayList<Integer>();
+        listaNros.add(18);
+        listaNros.add(5);
+        listaNros.add(10);
+        listaNros.add(17);
+        listaNros.add(3);
+        listaNros.add(31);
+        Set<Integer> numerosOrdenadosAsc = new TreeSet<>(listaNros);
         System.out.println("Numeros ordenados en orden ascendente: " + numerosOrdenadosAsc);
 
         Comparator<Integer> ordenDescendente = Comparator.reverseOrder();
         Set<Integer> numerosOrdenadosDesc = new TreeSet<>(ordenDescendente);
-        numerosOrdenadosDesc.addAll(numeros);
+        numerosOrdenadosDesc.addAll(listaNros);
         System.out.println("Numeros ordenados en orden descendente: " + numerosOrdenadosDesc);
 
+        //6. Cree una clase Persona con los atributos nombre, dni y edad. Luego debe
+        //crear otra clase que se encargue de la alta, baja y modificación de las
+        //personas. Esta última clase debe tener un HashMap de Personas donde la
+        //clave sea el dni y el valor el objeto Persona. Además de los métodos de ABM
+        //se debe contar con uno que agregue un objeto ya creado, otro que tome una
+        //lista de Personas y las agregue en el Map, y por último, un método que
+        //devuelva la estructura ordenada por edad. Pista: El HashMap no es una
+        //estructura de datos ordenada, por ende, el método no puede devolver este
+        //tipo de mapa.
+
+        Map<String, Persona> mapaPersonas = new HashMap<>();
+        for (int i=0; i<3; i++) {
+            mapaPersonas = ABM.alta(mapaPersonas);
+        }
+        mapaPersonas = ABM.baja(mapaPersonas, "35893486");
+        System.out.println(mapaPersonas.toString());
+        mapaPersonas = ABM.modificar(mapaPersonas, "6432443");
+        System.out.println(mapaPersonas.toString());
+        Persona p1 = new Persona("Diego", "20464844", 43);
+        mapaPersonas = ABM.agregarPersona(mapaPersonas, p1);
+        System.out.println(mapaPersonas.toString());
+        List<Persona> listaPersonas = new ArrayList<>();
+        listaPersonas.add(new Persona("Jorge", "39363834", 23));
+        listaPersonas.add(new Persona("Carla", "32363834", 35));
+        mapaPersonas = ABM.agregarListaPersonas(mapaPersonas, listaPersonas);
+        System.out.println(mapaPersonas.toString());
+        mapaPersonas = ABM.ordenarPorEdad(mapaPersonas);
+        for (Map.Entry<String, Persona> entry : mapaPersonas.entrySet()){
+            System.out.println("Nombre: " + entry.getValue().getNombre() + " , Edad: " + entry.getValue().getEdad());
+        }
     }
 
     public static List<Integer> generarNumerosAleatorios(int cantidad, int min, int max){
--- src/models/ABM.java
+++ src/models/ABM.java
@@ -1,13 +1,10 @@
 package models;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ABM {
-    private Map<String, Persona> personas = new HashMap<>();
 
-    public void alta(){
+    public static Map<String, Persona> alta(Map<String, Persona> personas) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el Nombre: ");
         String nombre = sc.nextLine();
@@ -16,19 +13,21 @@ public class ABM {
         System.out.println("Ingrese la Edad: ");
         int edad = sc.nextInt();
         Persona p = new Persona(nombre, dni, edad);
-        if(!(personas.containsKey(p.getDni()))){
+        if (!(personas.containsKey(p.getDni()))) {
             personas.put(p.getDni(), p);
-        }else System.out.println("ERROR. La persona ya se encuentra agregada en la base. ");
-
+        }
+        return personas;
     }
-    public void baja(String dni){
-        if(personas.containsKey(dni)){
+
+    public static Map<String, Persona> baja(Map<String, Persona> personas, String dni) {
+        if (personas.containsKey(dni)) {
             personas.remove(dni);
-        }else System.out.println("ERROR. El DNI ingresado no existe en la base de datos.");
-
+        }
+        return personas;
     }
-    public void modificar(String dni){
-        if(personas.containsKey(dni)){
+
+    public static Map<String, Persona> modificar(Map<String, Persona> personas, String dni) {
+        if (personas.containsKey(dni)) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Ingrese el Nombre: ");
             personas.get(dni).setNombre(sc.nextLine());
@@ -37,11 +36,48 @@ public class ABM {
             System.out.println("Ingrese la edad: ");
             personas.get(dni).setEdad(sc.nextInt());
         }
+        return personas;
     }
 
-    public void agregarPersona(Persona p){
-        if(personas.containsKey(p.getDni())){
+    public static Map<String, Persona> agregarPersona(Map<String, Persona> personas, Persona p) {
+        if (!personas.containsKey(p.getDni())) {
             personas.put(p.getDni(), p);
-        }else System.out.println("ERROR. La persona ya se encuentra agregada en la base. ");
+        }
+        return personas;
+    }
+
+    public static Map<String, Persona> agregarListaPersonas(Map<String, Persona> personas, List<Persona> lista) {
+        for (Persona p : lista) {
+            if (!personas.containsKey(p.getDni())) {
+                personas.put(p.getDni(), p);
+            }
+        }
+        return personas;
+    }
+
+    public static Map<String, Persona> ordenarPorEdad(Map<String, Persona> personas) {
+        Map<String, Persona> personasOrdenadas = new TreeMap<>(new ComparadorEdad(personas));
+
+        // Agregar todas las personas del Map original al TreeMap
+        personasOrdenadas.putAll(personas);
+
+        return personasOrdenadas;
+    }
+
+    static class ComparadorEdad implements Comparator<String> {
+        private Map<String, Persona> personas;
+
+        public ComparadorEdad(Map<String, Persona> personas) {
+            this.personas = personas;
+        }
+
+        @Override
+        public int compare(String dni1, String dni2) {
+            int edad1 = personas.get(dni1).getEdad();
+            int edad2 = personas.get(dni2).getEdad();
+
+            // Comparar por edad de forma descendente
+            return Integer.compare(edad1, edad2);
+        }
     }
 }
